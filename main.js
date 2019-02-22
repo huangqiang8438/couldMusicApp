96ed0a4a8e55dbc83d31f9350f73127310302cd2
hq-home <843804874@qq.com>
2019-02-22 16:05:29 +0800

    修改了下样式

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import 'font-awesome/css/font-awesome.css'
 Vue.config.productionTip = false
-
 App.mpType = 'app'
-
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
