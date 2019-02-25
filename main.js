be7c7bf4bcab103799281aeb5d722b2f374fc890
hq-home <843804874@qq.com>
2019-02-26 00:13:56 +0800

    注册全局组件

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App'
-
+import statusBar from '@/components/statusBar.vue'
 
 Vue.config.productionTip = false
 App.mpType = 'app'
-
+//app组件
+Vue.component('statusBar',statusBar)
 const app = new Vue({
 	...App
 })
